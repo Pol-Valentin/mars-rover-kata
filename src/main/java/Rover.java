9e62a287a025a10c2ad578316a2c18d92964e7fd
Pol-Valentin Cami <polvalentin@evaneos.com>
2016-08-30 16:33:36 +0200

    Created Rover with a default position

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -2,4 +2,19 @@
  * Created by pol on 30/08/16.
  */
 public class Rover {
+    private final int positionY;
+    private final int positionX;
+
+    public Rover(int positionX, int positionY) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
 }
--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -1,8 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by pol on 30/08/16.
  */
 public class RoverTest {
+
+    private Rover SUT;
+
+    @Before
+    public void before() {
+    }
+
+    @Test
+    public void it_should_be_initialized_at_a_position() {
+        SUT = new Rover(10, 5);
+        assertEquals(10, SUT.getPositionX());
+        assertEquals(5, SUT.getPositionY());
+    }
 
 }
